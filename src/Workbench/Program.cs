b62a246c44c223772f98655ac9beda0fd378ea4e
Andreas Hassmann <andreas@hassmann.com>
2020-05-13 23:10:28 +0200

    Comments in Workbench

--- src/Workbench/Program.cs
+++ src/Workbench/Program.cs
@@ -7,15 +7,18 @@ namespace Workbench
 {
 	public class Program
 	{
+		// Definitions
 		private const string deviceName = "Test Device";
 
 		private static GattID serviceUUID = "E6B4E8D6-00F2-44EE-B78D-4CF4330922BE";
 		private static GattID textUUID = "EAD840EE-4F73-4AC7-ACCA-13F8229D08D7";
 
+		// This is the handle to Bluetooth
 		private static BluetoothHost host;
 
 		// Definition of the GATT service(s)
-		private static GattService service = new GattService(serviceUUID,
+		private static GattService service = new GattService(
+			serviceUUID,
 			new TimeCharacteristic(
 				"Time", DateTime.UtcNow,
 				SigCharacteristic.CurrentTime,
@@ -45,13 +48,20 @@ namespace Workbench
 
 		public static void Main()
 		{
+			// Initialize the system, announce all services
 			host = BluetoothHost.Initialize(new BluetoothHostConfiguration(deviceName), service);
 
+			// Get notified
 			TextCharacteristic.ValueChanged += (characteristic) =>
 			{
-				Console.WriteLine($"Value changed: {TextCharacteristic.Name} = {TextCharacteristic.Value}");
+				// This gets called, when a connected phone changes a characteristic
+				Console.WriteLine($"Value changed by client: {TextCharacteristic.Name} = {TextCharacteristic.Value}");
+
+				// Just change the value to automatically notify a connected client
+				Text = "You sent: " + Text;
 			};
 
+			// You are visible as soon as you advertise
 			host.Advertise(advertisement);
 
 			Thread.Sleep(Timeout.Infinite);
